0862f54053182bcbaf3490ff4437adbdaed938d7
Tarcisio Neto <tarcisioneto1998@gmail.com>
2020-08-10 19:21:05 -0300

    Adicionado criação de modulo personalizado.

---
+++ area.js
@@ -0,0 +1,4 @@
+exports.area = function(width){
+    if(width === 3) return width*2;
+    return "foda-se";
+} //Criando um modulo
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 
+var Area = require('./area');
+
 var app = express();
 
+app.get('/sobre', function(req, res){
+    res.end("Hello World " + Area.area(2));
+});
+
 app.get('/', function(req, res){
-    res.end("Hello World");
+    res.end("Hello World " + Area.area(3));
 });
 
 app.listen(3000, function(){
-    console.log("O servidor está rodando");
+    console.log("O servidor está rodando. ");
 });
