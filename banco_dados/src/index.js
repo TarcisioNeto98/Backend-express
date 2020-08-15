268b80e7373e8815540210eff957a081de24c773
Tarcisio Neto <tarcisioneto1998@gmail.com>
2020-08-14 22:27:29 -0300

    Adicionado outros meios de conecção.

--- banco_dados/src/index.js
+++ banco_dados/src/index.js
@@ -4,7 +4,14 @@ var app = express();
 
 var conString = process.env.URL;
 
-banco = new banco.Pool({connectionString: conString});
+//banco = new banco.Pool({connectionString: conString});
+
+banco = new banco.Pool({user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.BD,
+  password: process.env.PASSWORD,
+  port: process.env.PORT}
+);
 
 app.get('/', function(req, res){
     banco.query('SELECT nome_aluno FROM teste.aluno', (err, result) => {
